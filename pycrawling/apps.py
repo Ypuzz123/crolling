b95635726e0dc24bcbce20fce547abb7214e92f0
user <a45436710@g.yju.ac.kr>
2023-12-01 23:01:55 +0900

    설명

--- pycrawling/apps.py
+++ pycrawling/apps.py
@@ -2,7 +2,6 @@ from django.apps import AppConfig
 from pycrawling import take_crawling
 #from .take_crawling import *
 
-
 class PycrawlingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'pycrawling'
